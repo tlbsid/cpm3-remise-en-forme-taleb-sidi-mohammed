391bb72931c93c2265a8d07f630bc67b9029a975
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-10-04 10:21:11 +0200

    Debug

--- exo3.js
+++ exo3.js
@@ -1,22 +1,23 @@
 // script.js
 const newTaskInput = document.getElementById('new-task');
-const addTaskButton = document.getElementById('add-task');
 const taskList = document.getElementById('tasks');
 
-addTaskButton.addEventListener('click', function () {
-    const taskText = newTaskInput.value.trim();
-    if (taskText !== '') {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <span>${taskText}</span>
-            <button class="delete">Supprimer</button>
-        `;
-        taskList.appendChild(li);
-        newTaskInput.value = '';
+newTaskInput.addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        const taskText = newTaskInput.value.trim();
+        if (taskText !== '') {
+            const li = document.createElement('li');
+            li.innerHTML = `
+                <span>${taskText}</span>
+                <button class="delete">Supprimer</button>
+            `;
+            taskList.appendChild(li);
+            newTaskInput.value = '';
 
-        const deleteButton = li.querySelector('.delete');
-        deleteButton.addEventListener('click', function () {
-            li.remove();
-        });
+            li.querySelector('.delete').addEventListener('click', function () {
+                li.remove();
+            });
+        }
     }
 });
+
